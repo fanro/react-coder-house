0632166dbbf6e8728ae08fd1c5f4617c6c21eedd
Max Power <fanro_87@hotmail.com>
2025-04-28 15:13:44 -0300

    avances

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,13 @@
 import { Flex } from '@chakra-ui/react';
 import logo from '../assets/img/logo.jpg';
+import apu from '../assets/img/apu_2.jpg';
 
 const ItemListContainer = ({ greeting }) => {
   return (
     <Flex direction='column' alignItems='center' justifyContent='center'>
       <h1>{greeting}</h1>
-      <img src={logo} className='logo react' alt='React logo' />
+      <img src={logo} style={{ width: '300px', height: '100px' }} alt='logo' />
+      <img src={apu} className='apu' alt='apu' />
     </Flex>
   );
 };
