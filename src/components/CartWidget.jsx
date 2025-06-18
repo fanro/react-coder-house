92ca0bbbc59c0184862c83043aa4976ae9eee238
Max Power <fanro_87@hotmail.com>
2025-06-17 23:03:16 -0300

    feat: cart context

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,15 @@
 import { Flex } from '@chakra-ui/react';
+import { useContext } from 'react';
 import { CiShoppingCart } from 'react-icons/ci';
+import { CartContext } from '../context/CartContext';
 
 const CartWidget = () => {
+  const { getTotalCount } = useContext(CartContext);
+  const totalCount = getTotalCount();
+
   return (
     <Flex alignItems='center'>
-      <CiShoppingCart size={30} /> 30
+      <CiShoppingCart size={30} /> {totalCount}
     </Flex>
   );
 };
